3ebbebe5d9277bca96229bf0ca6a72fb214968f4
chacha86 <taejincha0619@gmail.com>
2026-08-05 09:30:37 +0900

    4,5 강

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-h2console")
+    runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
--- src/main/java/com/back/p63260804/MainController.java
+++ src/main/java/com/back/p63260804/MainController.java
@@ -2,18 +2,24 @@ package com.back.p63260804;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 브라우저를 통해 해당 클래스의 함수를 호출할 수 잇따.
 public class MainController {
 
     @GetMapping("/")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("index() 메서드 호출됨!");
+        return "index() 메서드 호출됨!";
     }
 
     @GetMapping("/hello")
-    public void hello() {
-        System.out.println("hello() 메서드 호출됨!");
+    @ResponseBody
+    public String hello() {
+        System.out.println("hello() 메서드 호출됨!"); // 서버 콘솔
+        // 고객(요청한 쪽)의 브라우저에 출력
+        return "hello() 메서드 호출됨!";
     }
 
 
